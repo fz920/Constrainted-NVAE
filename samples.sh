69199c9c98ded7b5bce218b2cb78d6811d1dc360
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-15 21:06:07 +0100

    f

--- samples.sh
+++ samples.sh
@@ -1,6 +1,7 @@
-export EXPR_ID=9
+export EXPR_ID=11
 export DATA_DIR=/data/users/fz920/data
 export CHECKPOINT_DIR=/data/users/fz920/Constrainted-NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/Constrainted-NVAE
 
-python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --eval_mode=sample --temp=0.6 --readjust_bn
+python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --eval_mode=sample --temp=0.6 --readjust_bn \
+                   --save /data/users/fz920/expr
